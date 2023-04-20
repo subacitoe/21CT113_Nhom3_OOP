42e2ede9c79fe37c97cda4f8eed7fac0e40422b7
subacitoe <109783591+subacitoe@users.noreply.github.com>
2023-04-20 11:20:48 +0700

    20/04/2023

---
+++ src/QuanLyThucPham/ThucAnTuoi.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QuanLyThucPham;
+
+
+
+/**
+ *
+ * @author SUBACITO
+ */
+public class ThucAnTuoi extends ThucPham {
+    private String idKho;
+    private String idCtyNhap; 
+
+    public ThucAnTuoi(String idKho, String idCtyNhap) {
+        super();
+        this.idKho = idKho;
+        this.idCtyNhap = idCtyNhap;
+    }
+
+    public ThucAnTuoi() {
+        super();
+    }
+
+    public String getIdKho() {
+        return idKho;
+    }
+
+    public void setIdKho(String idKho) {
+        this.idKho = idKho;
+    }
+
+    public String getIdCtyNhap() {
+        return idCtyNhap;
+    }
+
+    public void setIdCtyNhap(String idCtyNhap) {
+        this.idCtyNhap = idCtyNhap;
+    }
+
+    @Override
+    public void input()
+    {
+        super.input();
+        sc.nextLine();
+        System.out.println("Nhap Kho: "); idKho = sc.nextLine();
+        System.out.println("Nhap Cty: "); idCtyNhap = sc.nextLine();
+
+    }
+    
+   @Override
+   public String toString()
+   {
+       return super.toString() + String.format("%25s | %25s |", this.idKho, this.idCtyNhap);
+   }
+    
+}
---
+++ src/QuanLyThucPham/ThucPham.java
@@ -0,0 +1,146 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QuanLyThucPham;
+
+/**
+ *
+ * @author SUBACITO
+ */
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+public class ThucPham {
+    static Scanner sc = new Scanner(System.in);
+//    Khai-bao
+    private String tenThucAn;
+    private String idThucAn;
+    private int soLuong;
+    private Double giaTien;
+    private Date nsx, hsd;
+    private String trangThai;
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+//Consrtuctor thamso
+    public ThucPham(String tenThucAn, String idThucAn, int soLuong, Double giaTien, Date nsx, Date hsd) {
+        this.tenThucAn = tenThucAn;
+        this.idThucAn = idThucAn;
+        this.soLuong = soLuong;
+        this.giaTien = giaTien;
+        this.nsx = nsx;
+        this.hsd = hsd;
+    }
+//Consrtuctor default
+    public ThucPham() {
+    }
+//    Getter va Setter
+
+
+
+    public String getTenThucAn() {
+        return tenThucAn;
+    }
+
+    public void setTenThucAn(String tenThucAn) {
+        this.tenThucAn = tenThucAn;
+    }
+
+    public String getIdThucAn() {
+        return idThucAn;
+    }
+
+    public void setIdThucAn(String idThucAn) {
+        this.idThucAn = idThucAn;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public Double getGiaTien() {
+        return giaTien;
+    }
+
+    public void setGiaTien(Double giaTien) {
+        this.giaTien = giaTien;
+    }
+
+    public Date getNsx() {
+        return nsx;
+    }
+
+    public void setNsx(Date nsx) {
+        this.nsx = nsx;
+    }
+
+    public Date getHsd() {
+        return hsd;
+    }
+
+    public void setHsd(Date hsd) {
+        this.hsd = hsd;
+    }
+    
+    public void setNSX(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        this.nsx = calendar.getTime();
+    }
+ 
+    
+    public void setHSD(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        this.hsd = calendar.getTime();
+    }
+    
+    public void kiemTraHSD() {
+        
+        Date today = new Date();
+        today.getTime();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String st = simpleDateFormat.format(today);
+        if (this.getHsd().compareTo(today) < 0) {
+//            System.out.print("Hien tai: " + st + ", thuc pham da het han! ");
+            setTrangThai("Bruh");
+        } else {
+//            System.out.print("Hien tai: " + st + ", thuc pham van con han ");
+            setTrangThai("OK");
+        }
+
+    }
+    public void input() {
+         System.out.println("Nhap ID: "); idThucAn = sc.nextLine();
+         System.out.println("Nhap ten: "); tenThucAn = sc.nextLine();
+         System.out.println("Nhap nsx: "); setNSX(sc.nextInt(), sc.nextInt(), sc.nextInt());
+         System.out.println("Nhap hsd: "); setHSD(sc.nextInt(), sc.nextInt(), sc.nextInt());
+         System.out.println("Nhap so luong:"); soLuong = sc.nextInt();
+         System.out.println("Nhap gia tien: "); giaTien = sc.nextDouble();     
+    }
+    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String date1 = simpleDateFormat.format(nsx);
+        String date2 = simpleDateFormat.format(hsd);
+        return String.format("%15s | %15s | %15s | %15s | %15s | %15s | %15s |", this.idThucAn, this.tenThucAn, this.soLuong, this.giaTien, date1, date2, this.trangThai);
+//        return idThucAn + "\t\t" + tenThucAn +"\t\t"+ soLuong +"\t"+ giaTien +"\t"+ date1 +"\t"+ date2 + "\t" + trangThai;
+    }
+   
+}
