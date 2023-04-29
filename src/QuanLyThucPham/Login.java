ae3ce021de3f710494dc2d5230eb21cc44cd8bf3
ndtl1701 <nguyenduongtanloi@gmail.com>
2023-04-29 22:58:46 +0700

    loi

--- src/QuanLyThucPham/Login.java
+++ src/QuanLyThucPham/Login.java
@@ -113,12 +113,20 @@ public class Login extends JFrame implements ActionListener {
         DeleteTatBtn.setBounds(1100, 100, 150, 40);
         menu.add(DeleteTatBtn);
 
+        JButton UpdateTatBtn = new JButton("Sửa TAT");
+        UpdateTatBtn.setBounds(1100, 150, 150, 40);
+        menu.add(UpdateTatBtn);
+
         JButton addTadnBtn = new JButton("Thêm");
         addTadnBtn.setBounds(1290, 50, 150, 40);
 
         JButton DeleteTADNBtn = new JButton("Xóa TADN");
         DeleteTADNBtn.setBounds(1290, 100, 150, 40);
         menu.add(DeleteTADNBtn);
+
+        JButton UpdateTADNBtn = new JButton("Sửa TADN");
+        UpdateTADNBtn.setBounds(1290, 150, 150, 40);
+        menu.add(UpdateTADNBtn);
 
         JLabel title = new JLabel("SUBACITO");
 //        title.setBounds(100, 50, 100, 30);
@@ -147,7 +155,24 @@ public class Login extends JFrame implements ActionListener {
         menu.setLocationRelativeTo(null);
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
- //nút xóa TAT 
+//nút update TAT
+        UpdateTatBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UpdateTAT();
+            }
+
+        });
+////nút update TADN
+        UpdateTADNBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UpdateTADN();
+            }
+
+        });
+
+        //nút xóa TAT 
         DeleteTatBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -156,7 +181,7 @@ public class Login extends JFrame implements ActionListener {
 
         });
 
- //nút xóa TADN
+        //nút xóa TADN
         DeleteTADNBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -457,8 +482,279 @@ public class Login extends JFrame implements ActionListener {
         MenuAddTADN.setLocationRelativeTo(null);
 
     }
-//Xóa TAT
+//Update TAT
 
+    public static void UpdateTAT() {
+        int VectorX = 10 * 30;
+        int VectorY = 60;
+        JFrame UpdateTAT = new JFrame("Sửa thông tin thức ăn tươi");
+
+        // thêm nút xác nhận
+        JButton ConfirmUpdatedTAT = new JButton("Xác nhận");
+        ConfirmUpdatedTAT.setBounds(VectorX / 2, VectorY * 9, 250, 30);
+        UpdateTAT.add(ConfirmUpdatedTAT);
+
+        // thêm nhập dữ liệu
+// ========================================================================
+        JTextField inputIdTAT = new JTextField();
+        inputIdTAT.setBounds(VectorX, VectorY * 1, 250, 30);
+        UpdateTAT.add(inputIdTAT);
+
+        JTextField inputNameTAT = new JTextField();
+        inputNameTAT.setBounds(VectorX, VectorY * 2, 250, 30);
+        UpdateTAT.add(inputNameTAT);
+
+        JTextField inputSlTAT = new JTextField();
+        inputSlTAT.setBounds(VectorX, VectorY * 3, 250, 30);
+        UpdateTAT.add(inputSlTAT);
+
+        JTextField inputCostTAT = new JTextField();
+        inputCostTAT.setBounds(VectorX, VectorY * 4, 250, 30);
+        UpdateTAT.add(inputCostTAT);
+//        ===============================================================================================       
+        JTextField inputNsxTATDay = new JTextField();
+        inputNsxTATDay.setBounds(VectorX, VectorY * 5, 50, 30);
+        UpdateTAT.add(inputNsxTATDay);
+
+        JTextField inputNsxTATMonth = new JTextField();
+        inputNsxTATMonth.setBounds(VectorX + 100, VectorY * 5, 50, 30);
+        UpdateTAT.add(inputNsxTATMonth);
+
+        JTextField inputNsxTATYear = new JTextField();
+        inputNsxTATYear.setBounds(VectorX + 200, VectorY * 5, 50, 30);
+        UpdateTAT.add(inputNsxTATYear);
+//        ===============================================================================================      
+        JTextField inputHsdTATDay = new JTextField();
+        inputHsdTATDay.setBounds(VectorX, VectorY * 6, 50, 30);
+        UpdateTAT.add(inputHsdTATDay);
+
+        JTextField inputHsdTATMonth = new JTextField();
+        inputHsdTATMonth.setBounds(VectorX + 100, VectorY * 6, 50, 30);
+        UpdateTAT.add(inputHsdTATMonth);
+
+        JTextField inputHsdTATYear = new JTextField();
+        inputHsdTATYear.setBounds(VectorX + 200, VectorY * 6, 50, 30);
+        UpdateTAT.add(inputHsdTATYear);
+//        ===============================================================================================             
+        JTextField inputNhietDoTuDong = new JTextField();
+        inputNhietDoTuDong.setBounds(VectorX, VectorY * 7, 250, 30);
+        UpdateTAT.add(inputNhietDoTuDong);
+
+        JTextField inputIDTuDong = new JTextField();
+        inputIDTuDong.setBounds(VectorX, VectorY * 8, 250, 30);
+        UpdateTAT.add(inputIDTuDong);
+
+        // thêm label
+        JLabel LabelIdTAT = new JLabel("ID: ");
+        LabelIdTAT.setBounds(VectorX / 2, VectorY * 1, 250, 30);
+        UpdateTAT.add(LabelIdTAT);
+
+        JLabel LabelNameTAT = new JLabel("Tên thực phẩm: ");
+        LabelNameTAT.setBounds(VectorX / 2, VectorY * 2, 250, 30);
+        UpdateTAT.add(LabelNameTAT);
+
+        JLabel LabelSlTAT = new JLabel("Số lượng: ");
+        LabelSlTAT.setBounds(VectorX / 2, VectorY * 3, 250, 30);
+        UpdateTAT.add(LabelSlTAT);
+
+        JLabel LabelCostTAT = new JLabel("Giá: ");
+        LabelCostTAT.setBounds(VectorX / 2, VectorY * 4, 250, 30);
+        UpdateTAT.add(LabelCostTAT);
+
+        JLabel LabelNsxTAT = new JLabel("Ngày sản xuất: ");
+        LabelNsxTAT.setBounds(VectorX / 2, VectorY * 5, 250, 30);
+        UpdateTAT.add(LabelNsxTAT);
+
+        JLabel LabelHsdTAT = new JLabel("Hạn sử dụng: ");
+        LabelHsdTAT.setBounds(VectorX / 2, VectorY * 6, 250, 30);
+        UpdateTAT.add(LabelHsdTAT);
+
+        JLabel LabelKhoTAT = new JLabel("Nhiệt độ tủ đông: ");
+        LabelKhoTAT.setBounds(VectorX / 2, VectorY * 7, 250, 30);
+        UpdateTAT.add(LabelKhoTAT);
+
+        JLabel LabelCtyTAT = new JLabel("ID Tủ đông: ");
+        LabelCtyTAT.setBounds(VectorX / 2, VectorY * 8, 250, 30);
+        UpdateTAT.add(LabelCtyTAT);
+
+        ConfirmUpdatedTAT.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String StringInputIdTAT = inputIdTAT.getText();
+                String StringInputNameTAT = inputNameTAT.getText();
+                int NumberInputSlTAT = Integer.parseInt(inputSlTAT.getText());
+                double NumerInputCostTAT = Double.parseDouble(inputCostTAT.getText());
+
+                int InputNsxTATDay = Integer.parseInt(inputNsxTATDay.getText());
+                int InputNsxTATMonth = Integer.parseInt(inputNsxTATMonth.getText());
+                int InputNsxTATYear = Integer.parseInt(inputNsxTATYear.getText());
+
+                int InputHsdTATDay = Integer.parseInt(inputHsdTATDay.getText());
+                int InputHsdTATMonth = Integer.parseInt(inputHsdTATMonth.getText());
+                int InputHsdTATYear = Integer.parseInt(inputHsdTATYear.getText());
+
+                double StringInputKhoTAT = Double.parseDouble(inputNhietDoTuDong.getText());
+                String StringInputCtyTAT = inputIDTuDong.getText();
+
+                ThucAnTuoi Tat = new ThucAnTuoi();
+                Tat.setIdThucAn(StringInputIdTAT);
+                Tat.setTenThucAn(StringInputNameTAT);
+                Tat.setSoLuong(NumberInputSlTAT);
+                Tat.setGiaTien(NumerInputCostTAT);
+                Tat.setNSX(InputNsxTATYear, InputNsxTATMonth, InputNsxTATDay);
+                Tat.setHSD(InputHsdTATYear, InputHsdTATMonth, InputHsdTATDay);
+                Tat.setNhietDoTuDong(StringInputKhoTAT);
+                Tat.setIDTuDong(StringInputCtyTAT);
+                tat.add(Tat);
+
+                UpdateTAT.dispose();
+            }
+        });
+
+        UpdateTAT.setSize(600, 650);
+        UpdateTAT.setLayout(null);
+        UpdateTAT.setVisible(true);
+        UpdateTAT.setResizable(false);
+        UpdateTAT.setLocationRelativeTo(null);
+    }
+
+//Update TADN
+    public static void UpdateTADN() {
+        int VectorX = 10 * 30;
+        int VectorY = 60;
+        JFrame UpdateTADN = new JFrame("Sửa thông tin thức ăn dài ngày");
+
+        // thêm nút xác nhận
+        JButton ConfirmUpdatedTADN = new JButton("Xác nhận");
+        ConfirmUpdatedTADN.setBounds(VectorX / 2, VectorY * 9, 250, 30);
+        UpdateTADN.add(ConfirmUpdatedTADN);
+
+        // thêm nhập dữ liệu
+// ========================================================================
+        JTextField inputIdTADN = new JTextField();
+        inputIdTADN.setBounds(VectorX, VectorY * 1, 250, 30);
+        UpdateTADN.add(inputIdTADN);
+
+        JTextField inputNameTADN = new JTextField();
+        inputNameTADN.setBounds(VectorX, VectorY * 2, 250, 30);
+        UpdateTADN.add(inputNameTADN);
+
+        JTextField inputSlTADN = new JTextField();
+        inputSlTADN.setBounds(VectorX, VectorY * 3, 250, 30);
+        UpdateTADN.add(inputSlTADN);
+
+        JTextField inputCostTADN = new JTextField();
+        inputCostTADN.setBounds(VectorX, VectorY * 4, 250, 30);
+        UpdateTADN.add(inputCostTADN);
+
+        //===============================================================================================       
+        JTextField inputNsxTADNDay = new JTextField();
+        inputNsxTADNDay.setBounds(VectorX, VectorY * 5, 50, 30);
+        UpdateTADN.add(inputNsxTADNDay);
+
+        JTextField inputNsxTADNMonth = new JTextField();
+        inputNsxTADNMonth.setBounds(VectorX + 100, VectorY * 5, 50, 30);
+        UpdateTADN.add(inputNsxTADNMonth);
+
+        JTextField inputNsxTADNYear = new JTextField();
+        inputNsxTADNYear.setBounds(VectorX + 200, VectorY * 5, 50, 30);
+        UpdateTADN.add(inputNsxTADNYear);
+        //===============================================================================================      
+        JTextField inputHsdTADNDay = new JTextField();
+        inputHsdTADNDay.setBounds(VectorX, VectorY * 6, 50, 30);
+        UpdateTADN.add(inputHsdTADNDay);
+
+        JTextField inputHsdTADNMonth = new JTextField();
+        inputHsdTADNMonth.setBounds(VectorX + 100, VectorY * 6, 50, 30);
+        UpdateTADN.add(inputHsdTADNMonth);
+
+        JTextField inputHsdTADNYear = new JTextField();
+        inputHsdTADNYear.setBounds(VectorX + 200, VectorY * 6, 50, 30);
+        UpdateTADN.add(inputHsdTADNYear);
+        //===============================================================================================   
+
+        JTextField inputDoAmTADN = new JTextField();
+        inputDoAmTADN.setBounds(VectorX, VectorY * 7, 250, 30);
+        UpdateTADN.add(inputDoAmTADN);
+
+        JTextField inputNhietDoKhoTADN = new JTextField();
+        inputNhietDoKhoTADN.setBounds(VectorX, VectorY * 8, 250, 30);
+        UpdateTADN.add(inputNhietDoKhoTADN);
+
+        // thêm label
+        JLabel LabelIdTADN = new JLabel("ID: ");
+        LabelIdTADN.setBounds(VectorX / 2, VectorY * 1, 250, 30);
+        UpdateTADN.add(LabelIdTADN);
+
+        JLabel LabelNameTADN = new JLabel("Tên thực phẩm: ");
+        LabelNameTADN.setBounds(VectorX / 2, VectorY * 2, 250, 30);
+        UpdateTADN.add(LabelNameTADN);
+
+        JLabel LabelSlTADN = new JLabel("Số lượng: ");
+        LabelSlTADN.setBounds(VectorX / 2, VectorY * 3, 250, 30);
+        UpdateTADN.add(LabelSlTADN);
+
+        JLabel LabelCostTADN = new JLabel("Giá: ");
+        LabelCostTADN.setBounds(VectorX / 2, VectorY * 4, 250, 30);
+        UpdateTADN.add(LabelCostTADN);
+
+        JLabel LabelNsxTADN = new JLabel("Ngày sản xuất: ");
+        LabelNsxTADN.setBounds(VectorX / 2, VectorY * 5, 250, 30);
+        UpdateTADN.add(LabelNsxTADN);
+
+        JLabel LabelHsdTADN = new JLabel("Hạn sử dụng: ");
+        LabelHsdTADN.setBounds(VectorX / 2, VectorY * 6, 250, 30);
+        UpdateTADN.add(LabelHsdTADN);
+
+        JLabel LabelDoAmTADN = new JLabel("Độ ẩm: ");
+        LabelDoAmTADN.setBounds(VectorX / 2, VectorY * 7, 250, 30);
+        UpdateTADN.add(LabelDoAmTADN);
+
+        JLabel LabelNDKhoTAT = new JLabel("Nhiệt độ kho: ");
+        LabelNDKhoTAT.setBounds(VectorX / 2, VectorY * 8, 250, 30);
+        UpdateTADN.add(LabelNDKhoTAT);
+
+        ConfirmUpdatedTADN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String StringInputIdTADN = inputIdTADN.getText();
+                String StringInputNameTADN = inputNameTADN.getText();
+                int NumberInputSlTADN = Integer.parseInt(inputSlTADN.getText());
+                double NumerInputCostTADN = Double.parseDouble(inputCostTADN.getText());
+
+                int InputNsxTADNDay = Integer.parseInt(inputNsxTADNDay.getText());
+                int InputNsxTADNMonth = Integer.parseInt(inputNsxTADNMonth.getText());
+                int InputNsxTADNYear = Integer.parseInt(inputNsxTADNYear.getText());
+
+                int InputHsdTADNDay = Integer.parseInt(inputHsdTADNDay.getText());
+                int InputHsdTADNMonth = Integer.parseInt(inputHsdTADNMonth.getText());
+                int InputHsdTADNYear = Integer.parseInt(inputHsdTADNYear.getText());
+
+                double StringInputDoAmTADN = Double.parseDouble(inputDoAmTADN.getText());
+                double InputNhietDoKhoTADN = Double.parseDouble(inputNhietDoKhoTADN.getText());
+
+                ThucAnDaiNgay Tadn = new ThucAnDaiNgay();
+                Tadn.setIdThucAn(StringInputIdTADN);
+                Tadn.setTenThucAn(StringInputNameTADN);
+                Tadn.setSoLuong(NumberInputSlTADN);
+                Tadn.setGiaTien(NumerInputCostTADN);
+                Tadn.setNSX(InputNsxTADNYear, InputNsxTADNMonth, InputNsxTADNDay);
+                Tadn.setHSD(InputHsdTADNYear, InputHsdTADNMonth, InputHsdTADNDay);
+                Tadn.setDoAm(StringInputDoAmTADN);
+                Tadn.setNhietDoKho(InputNhietDoKhoTADN);
+                tadn.add(Tadn);
+                UpdateTADN.dispose();
+            }
+        });
+
+        UpdateTADN.setSize(600, 650);
+        UpdateTADN.setLayout(null);
+        UpdateTADN.setVisible(true);
+        UpdateTADN.setResizable(false);
+        UpdateTADN.setLocationRelativeTo(null);
+    }
+
+//Xóa TAT
     public static void DeleteTAT() {
         int VectorX = 10 * 30;
         int VectorY = 60;
@@ -499,7 +795,7 @@ public class Login extends JFrame implements ActionListener {
         DeleteTAT.setLocationRelativeTo(null);
     }
 
-    //Xóa TAT
+    //Xóa TADN
     public static void DeleteTADN() {
         int VectorX = 10 * 30;
         int VectorY = 60;
